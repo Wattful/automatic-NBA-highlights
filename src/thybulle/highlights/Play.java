b06c35124b55a36794d4d4c7621c7118251d0c81
unknown <theodkul@gmail.com>
2020-08-20 20:33:27 -0400

    Improve Play field names

--- src/thybulle/highlights/Play.java
+++ src/thybulle/highlights/Play.java
@@ -11,11 +11,11 @@ import thybulle.misc.*;
 public abstract class Play {
 	private static final boolean CHECK_REP = true;
 
-	private final PlayType pt;
-	private final List<Player> p;
-	private final Timestamp t;
-	private final Team te;
-	private final Score s;
+	private final PlayType playType;
+	private final List<Player> players;
+	private final Timestamp timestamp;
+	private final Team team;
+	private final Score score;
 
 	/**Constructs a Play with the specified fields.
 	@throws NullPointerException if any arguments are null.
@@ -27,11 +27,11 @@ public abstract class Play {
 		if(player.size() != playType.getNumberOfPlayers()){
 			throw new IllegalArgumentException("Number of players did not match the number required for the play type.");
 		}
-		this.pt = playType;
-		this.p = List.copyOf(player);
-		this.t = timestamp;
-		this.te = team;
-		this.s = score;
+		this.playType = playType;
+		this.players = List.copyOf(player);
+		this.timestamp = timestamp;
+		this.team = team;
+		this.score = score;
 	}
 
 	/**Returns a video object containing footage of this play.
@@ -43,35 +43,35 @@ public abstract class Play {
 	@return the type of this play.
 	*/
 	public PlayType getType(){
-		return this.pt;
+		return this.playType;
 	}
 
 	/**Returns the players that are associated with this play.
 	@return the players that are associated with this play.
 	*/
 	public List<Player> getPlayers(){
-		return this.p;
+		return this.players;
 	}
 
 	/**Returns the time that this play occured at.
 	@return the time that this play occured at.
 	*/
 	public Timestamp getTimestamp(){
-		return this.t;
+		return this.timestamp;
 	}
 
 	/**Returns the team that committed this play.
 	@return the team that committed this play.
 	*/
 	public Team getTeam(){
-		return this.te;
+		return this.team;
 	}
 
 	/**Returns the current score at the time of this play.
 	@return the current score at the time of this play.
 	*/
 	public Score getScore(){
-		return this.s;
+		return this.score;
 	}
 
 	@Override
@@ -79,7 +79,7 @@ public abstract class Play {
 	@return a hash code for this Play.
 	*/
 	public int hashCode(){
-		return this.pt.hashCode() + this.p.hashCode() + this.t.hashCode() + this.te.hashCode() + this.s.hashCode();
+		return this.playType.hashCode() + this.players.hashCode() + this.timestamp.hashCode() + this.team.hashCode() + this.score.hashCode();
 	}
 
 	@Override
@@ -100,7 +100,7 @@ public abstract class Play {
 			return false;
 		}
 		Play pl = (Play)o;
-		return this.pt.equals(pl.pt) && this.p.equals(pl.p) && this.t.equals(pl.t) && this.te.equals(pl.te) && this.s.equals(pl.s);
+		return this.playType.equals(pl.playType) && this.players.equals(pl.players) && this.timestamp.equals(pl.timestamp) && this.team.equals(pl.team) && this.score.equals(pl.score);
 	}
 
 	@Override
@@ -108,6 +108,6 @@ public abstract class Play {
 	@return a String representation of this Play.
 	*/
 	public String toString(){
-		return this.pt.toString() + " by " + (this.p.size() > 0 ? (this.p.toString() + " of ") : "") + this.te.toString() + " at " + this.t.toString();
+		return this.playType.toString() + " by " + (this.players.size() > 0 ? (this.players.toString() + " of ") : "") + this.team.toString() + " at " + this.timestamp.toString();
 	}
 }
